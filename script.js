3e38a1d48cb099b8b000348c6dcc7a73280ae177
Naman Khare <namankhare329@gmail.com>
2026-08-20 21:50:10 +0530

    Adding the javaScript file

---
+++ script.js
@@ -0,0 +1,7 @@
+const topSection = document.querySelector(".Top-section");
+
+const pinkBtn = document.querySelector("#pink");
+const blueBtn = document.querySelector("#blue");
+const purpleBtn = document.querySelector("#purple");
+const greenBtn = document.querySelector("#green");
+const yellowBtn = document.querySelector("#yellow");
