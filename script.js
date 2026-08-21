c30d4b4330dab8c281b63d4ea7494c20b240fd59
Naman Khare <namankhare329@gmail.com>
2026-08-21 12:30:42 +0530

    Add the funnctionality of the dynnamic colors

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const topSection = document.querySelector(".Top-section");
 const mainHeading = document.querySelector("#main-heading");
+const tertText = document.querySelectorAll(".tert-text");
+const priBtbn = document.querySelector(".Primary-btn");
+const secBtn = document.querySelector(".Secondary-btn");
 
 const pinkBtn = document.querySelector("#pink");
 const blueBtn = document.querySelector("#blue");
@@ -11,24 +14,54 @@ const yellowBtn = document.querySelector("#yellow");
 pinkBtn.addEventListener("click" , () => {
     topSection.style.backgroundColor = "#FD1857";
     mainHeading.style.color = "#FD1857";
+    tertText.forEach((box) => {
+        box.style.color = "#FD1857";
+    });
+    priBtbn.style.backgroundColor = "#FD1857";
+    secBtn.style.borderColor = "#FD1857";
+    secBtn.style.color = "#FD1857";
 });
 
 blueBtn.addEventListener("click" , () => {
     topSection.style.backgroundColor = "#3498DB";
     mainHeading.style.color = "#3498DB";
+    tertText.forEach((box) => {
+        box.style.color = "#3498DB";
+    });
+    priBtbn.style.backgroundColor = "#3498DB";
+    secBtn.style.borderColor = "#3498DB";
+    secBtn.style.color = "#3498DB";
 });
 
 purpleBtn.addEventListener("click" , () => {
     topSection.style.backgroundColor = "#9B59B6";
     mainHeading.style.color = "#9B59B6";
+    tertText.forEach((box) => {
+        box.style.color = "#9B59B6";
+    });
+    priBtbn.style.backgroundColor = "#9B59B6";
+    secBtn.style.borderColor = "#9B59B6";
+    secBtn.style.color = "#9B59B6";
 });
 
 greenBtn.addEventListener("click" , () => {
     topSection.style.backgroundColor = "#2ECC71";
     mainHeading.style.color = "#2ECC71";
+    tertText.forEach((box) => {
+        box.style.color = "#2ECC71";
+    });
+    priBtbn.style.backgroundColor = "#2ECC71";
+    secBtn.style.borderColor = "#2ECC71";
+    secBtn.style.color = "#2ECC71";
 });
 
 yellowBtn.addEventListener("click" , () => {
     topSection.style.backgroundColor = "#F1C40F";
     mainHeading.style.color = "#F1C40F";
+    tertText.forEach((box) => {
+        box.style.color = "#F1C40F";
+    });
+    priBtbn.style.backgroundColor = "#F1C40F";
+    secBtn.style.borderColor = "#F1C40F";
+    secBtn.style.color = "#F1C40F";
 });
